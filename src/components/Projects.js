e7b472f567332385cd431b540bb8c1f15361a52f
leonnn124 <s0976144676s@gmail.com>
2023-02-23 19:04:18 +0800

    add new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import fitclub from "../assets/fitclub.jpg";
 import deliveroo from "../assets/deliveroo.jpg";
+import metaverse from "../assets/metaverse.jpg";
 
 const Projects = () => {
   return (
@@ -47,6 +48,31 @@ const Projects = () => {
             <div className="text-sm tracking-[2px]">
               <a
                 href="https://www.youtube.com/watch?v=duB_4g5SpVk"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View {">"}
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-screen snap-center flex-shrink-0 flex lg:flex-row flex-col items-center justify-center ">
+          <img
+            src={metaverse}
+            alt="metaverse"
+            className="xl:w-[60%] xl:h-[75%] lg:w-[50%] lg:h-[65%] sm:h-[50%] w-[90%]  object-contain"
+          />
+          <div className="xl:w-[20%] lg:w-[20%] text-center lg:ml-6 space-y-8  w-[90%]">
+            <h2 className="font-burtons text-3xl mt-6 text-teal-600">
+              Metaverse
+            </h2>
+            <p className="text-sm tracking-[2px]">
+              #React #Tailwind #Framer Motion
+            </p>
+            <div className="text-sm tracking-[2px]">
+              <a
+                href="https://leonnnx124-metaverse.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
